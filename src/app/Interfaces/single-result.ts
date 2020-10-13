eb7c14ba4e217f7d1c1d33f2487d29a0442107a5
Aaron Rudzki <aaronrudzki@quickenloans.com>
2020-10-13 16:42:49 -0400

    Initial Commit

---
+++ src/app/Interfaces/single-result.ts
@@ -0,0 +1,5 @@
+export interface SingleResult {
+    title: string;
+    image: string;
+    link: string;
+}
---
+++ src/app/aww-view/aww-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aww-view',
+  templateUrl: './aww-view.component.html',
+  styleUrls: ['./aww-view.component.css']
+})
+export class AwwViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
